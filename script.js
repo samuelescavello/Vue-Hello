b1f0ad8297a9068dc48917da38209c6448854131
samuelescavello <scavellosamuele23@gmail.com>
2024-03-27 16:47:12 +0100

    prove varie

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ createApp({
     data(){
         return{
            message : "<h1>hello vue!</h1>",
+           messageTwo : "<h3>speriamo di imparare vue</h3>",
+           mexFoto : "<h4>qui sotto troverai una foto</h4>",
            foto : "img/stranger.jpeg",
            altfoto : 'stranger'
         }
