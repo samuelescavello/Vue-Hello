03325ddf5e91e4cb560b5d5bb365385adaf6a9d0
samuelescavello <scavellosamuele23@gmail.com>
2024-03-27 14:49:03 +0100

    vue

---
+++ script.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue ;
+createApp({
+    data(){
+        return{
+           message : "<h1>hello vue!</h1>",
+           foto : "img/stranger.jpeg",
+        }
+    },
+
+}).mount('#app');
+
+
+
+
+
+
+
+
